d7d4122937d626ba93e17b009965f81051f09e57
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-10-14 14:55:56 -0400

    Adds lazy buttons and stack view properties

--- Animations-Two/Animations-Two/ViewController.swift
+++ Animations-Two/Animations-Two/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var buttonStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [linearButton, easeInButton, easeOutButton, easeInEaseOutButton])
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    lazy var linearButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Linear", for: .normal)
+        return button
+    }()
+    
+    lazy var easeInButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("EaseIn", for: .normal)
+        return button
+    }()
+    
+    lazy var easeOutButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("EaseOut", for: .normal)
+        return button
+    }()
+    
+    lazy var easeInEaseOutButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("EaseInEastOut", for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
