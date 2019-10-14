058e57541ca59fb81fec8384dc9cbef4f8d8be1e
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-10-14 15:19:08 -0400

    adds constraints to buttonstackview

--- Animations-Two/Animations-Two/ViewController.swift
+++ Animations-Two/Animations-Two/ViewController.swift
@@ -14,37 +14,78 @@ class ViewController: UIViewController {
         let stackView = UIStackView(arrangedSubviews: [linearButton, easeInButton, easeOutButton, easeInEaseOutButton])
         stackView.axis = .horizontal
         stackView.alignment = .center
+        stackView.distribution = .fillEqually
         return stackView
     }()
     
     lazy var linearButton: UIButton = {
         let button = UIButton()
         button.setTitle("Linear", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.showsTouchWhenHighlighted = true
+        button.isUserInteractionEnabled = true
+        button.titleLabel?.font = .systemFont(ofSize: 12)
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
         return button
     }()
     
     lazy var easeInButton: UIButton = {
         let button = UIButton()
         button.setTitle("EaseIn", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.showsTouchWhenHighlighted = true
+        button.isUserInteractionEnabled = true
+        button.titleLabel?.font = .systemFont(ofSize: 12)
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
         return button
     }()
     
     lazy var easeOutButton: UIButton = {
         let button = UIButton()
         button.setTitle("EaseOut", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.showsTouchWhenHighlighted = true
+        button.isUserInteractionEnabled = true
+        button.titleLabel?.font = .systemFont(ofSize: 12)
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
         return button
     }()
     
     lazy var easeInEaseOutButton: UIButton = {
         let button = UIButton()
         button.setTitle("EaseInEastOut", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.showsTouchWhenHighlighted = true
+        button.isUserInteractionEnabled = true
+        button.titleLabel?.font = .systemFont(ofSize: 12)
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
         return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        addSubviews()
+        addConstraints()
     }
 
+    private func addSubviews() {
+        self.view.addSubview(buttonStackView)
+    }
+    
+    private func addConstraints() {
+        configureButtonStackViewConstraints()
+    }
+    
+    private func configureButtonStackViewConstraints() {
+        self.buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.buttonStackView.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor),
+            self.buttonStackView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            self.buttonStackView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 5),
+            self.buttonStackView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -5)
+        ])
+    }
 }
 
